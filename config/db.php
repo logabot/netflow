75ffd9bd774562d7e4ed59021c1ca4ebd3320866
root <root@yii.home.lan>
2016-03-28 02:43:18 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=netflow_test',
+    'username' => 'test2',
+    'password' => 'dima403',
+    'charset' => 'utf8',
+];
